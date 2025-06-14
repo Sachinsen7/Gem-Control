bd63f86a5aff026279ff0dbba8165fe27c58fb45
Sachin Sen <sachinsen1920@gmail.com>
2025-06-14 21:06:00 +0530

    Refactor DbConnection.js: standardize code formatting and improve readability of MongoDB connection logic

--- Backend/Config/DbConnection.js
+++ Backend/Config/DbConnection.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-        });
-        console.log('MongoDB connected successfully');
-    } catch (error) {
-        console.error('MongoDB connection failed:', error.message);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // Exit process with failure
+  }
 };
-connectDB();
 
-
-
+module.exports = connectDB; // Export the function
