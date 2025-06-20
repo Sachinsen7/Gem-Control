d8a81cc88bfa3aa53c1e9e0e94e7fe9b966c2303
Sachin Sen <Sachinsen1920@gmail.com>
2025-06-20 22:08:40 +0530

    add missing raw material routes to AdminRoutes

--- Backend/Routes/AdminRoutes.js
+++ Backend/Routes/AdminRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { upload } = require("../Utils/UploadFile.js");
 const { isLoggedIn, isAdmin, isStaff } = require("../Utils/islogedin");
+
 const {
   RegisterUser,
   GetAllUsers,
@@ -22,6 +23,12 @@ const {
   removeStock,
   getStockbyCategory,
   getStockbyFirm,
+  createRawMaterial,
+  getAllRawMaterials,
+  getRawMaterialbyFirm,
+  getRawMaterialbyType,
+  removeRawMaterial,
+  AddRawMaterialStock,
 } = require("../Controllers/adminController");
 
 router.post("/register", RegisterUser);
