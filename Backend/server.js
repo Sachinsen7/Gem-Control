ae1fc456668da7f8cfe01a6649526c0e5017abca
Siddhantwork099 <siddhantwork867@gmail.com>
2025-09-10 10:59:55 +0530

    server

--- Backend/server.js
+++ Backend/server.js
@@ -29,9 +29,9 @@ app.use(express.static(path.join(__dirname, "../Frontend-gem/GemControl/dist")))
 // console.log("Serving static files from:", path.join(__dirname, "../Frontend-gem/GemControl/dist"));
 
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../Frontend-gem/GemControl/dist" + "/index.html")
+    path.join(__dirname, "../Frontend-gem/GemControl/dist/index.html")
   );
 });
 
